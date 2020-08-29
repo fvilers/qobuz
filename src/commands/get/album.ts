9045173c467305ae8cd17ebfc1e09ca944607184
Fabian Vilers <fabian.vilers@dev-one.com>
2020-08-29 19:52:45 +0200

    Display more information about album

--- src/commands/get/album.ts
+++ src/commands/get/album.ts
@@ -1,6 +1,20 @@
+import chalk from "chalk";
+import { getBorderCharacters, table } from "table";
+import Track from "../../models/Track";
 import QobuzClient from "../../QobuzClient";
 
 type Arguments = { id: string };
+
+function getAlbumDuration(tracks: Track[]): string {
+  const total = tracks.reduce((prev, curr) => prev + curr.duration, 0);
+  const hours = Math.floor(total / 3600);
+  const totalWithoutHours = total % 3600;
+  const minutes = Math.floor(totalWithoutHours / 60);
+  const seconds = totalWithoutHours % 60;
+
+  // TODO : don't display hours if 0
+  return `${hours}h ${minutes}m ${seconds}s`;
+}
 
 export const command = "album <id>";
 export const description = "Return metadata for an album";
@@ -8,6 +22,31 @@ export const builder = {};
 export const handler = async ({ id }: Arguments) => {
   const client = new QobuzClient();
   const album = await client.album.get(id);
+  const rows = [
+    [chalk.gray("ID"), album.id],
+    [chalk.gray("Title"), album.title],
+    [chalk.gray("Artist"), album.artist.name],
+    [
+      chalk.gray("Description"),
+      // TODO : format date
+      `Published by ${album.label.name} on ${album.release_date_original}`,
+    ],
+    [
+      "",
+      `${album.genre.name} - ${album.tracks_count} tracks - ${getAlbumDuration(
+        album.tracks.items
+      )}`,
+    ],
+  ];
 
-  console.log(album.title);
+  console.log(
+    table(rows, {
+      border: getBorderCharacters(`void`),
+      columnDefault: {
+        paddingLeft: 0,
+        paddingRight: 1,
+      },
+      drawHorizontalLine: () => false,
+    })
+  );
 };
