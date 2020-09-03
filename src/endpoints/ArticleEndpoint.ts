f98ded77459ed057a46a606902359656684cb25a
Fabian Vilers <fabian.vilers@dev-one.com>
2020-09-03 08:45:46 +0200

    Add the article endpoint's getRelatedContent method

--- src/endpoints/ArticleEndpoint.ts
+++ src/endpoints/ArticleEndpoint.ts
@@ -1,6 +1,14 @@
+import Album from "../models/Album";
 import Article from "../models/Article";
+import Artist from "../models/Artist";
 import QobuzClient from "../QobuzClient";
 import QueryParameters from "../types/QueryParameters";
+
+type GetRelatedContentResponse = {
+  albums: Album[];
+  artists: Artist[];
+  id: string;
+};
 
 class ArticleEndpoint {
   constructor(private readonly client: QobuzClient) {}
@@ -9,6 +17,15 @@ class ArticleEndpoint {
     const parameters: QueryParameters = { article_id: articleId };
 
     return this.client.get<Article>("article/get", parameters);
+  }
+
+  getRelatedContent(articleId: string): Promise<GetRelatedContentResponse> {
+    const parameters: QueryParameters = { article_id: articleId };
+
+    return this.client.get<GetRelatedContentResponse>(
+      "article/getRelatedContent",
+      parameters
+    );
   }
 }
 
